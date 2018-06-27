0aef937c4f502d8dfba27796e68c1ef18719733a
alireza fassih <fassih.alireza@gmail.com>
2018-06-27 21:50:32 +0430

    Initial commit

---
+++ src/main/java/ir/fassih/workshopautomation/WorkshopAutomationApplication.java
@@ -0,0 +1,12 @@
+package ir.fassih.workshopautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopAutomationApplication.class, args);
+	}
+}
